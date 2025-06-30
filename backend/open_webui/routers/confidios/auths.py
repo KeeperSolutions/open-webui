6c2819614fe10407dd0e5ec19e21a393f4cd9ced
ahadrovic-2 <antonio.hadrovic@keepersolutions.com>
2025-06-30 13:19:58 +0200

    Add admin check to confidios login

--- backend/open_webui/routers/confidios/auths.py
+++ backend/open_webui/routers/confidios/auths.py
@@ -17,12 +17,17 @@ router = APIRouter()
 #     # Your authenticated endpoint logic here
 #     return {"features": []}
 
-# todo: now only logged in users can access this endpoint
+# todo: now only logged in admin users can access this endpoint
 
 
 @router.post("/auths/login")
 async def confidios_admin_login(user=Depends(get_verified_user)):
-    # Your authenticated endpoint logic here
+    if user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Only admin users can access this endpoint",
+        )
+
     return {
         "status": f"confidios feature accessed by user: {user.email}",
         "user_id": user.id,
