b50416e142bbf655b54c2772e76600d242be7c43
ahadrovic-2 <antonio.hadrovic@keepersolutions.com>
2026-01-15 12:23:37 +0100

    Fix sidebar overlap, silence expired token notification prior to auth redirect

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -2409,9 +2409,9 @@
 />
 
 <div
-	class="h-screen max-h-[100dvh] transition-width duration-200 ease-in-out {$showSidebar
-		? '  md:max-w-[calc(100%-var(--sidebar-width))]'
-		: ' '} w-full max-w-full flex flex-col"
+	class="h-screen max-h-[100dvh] transition-all duration-200 ease-in-out {$showSidebar
+		? 'md:ml-[var(--sidebar-width)] md:max-w-[calc(100%-var(--sidebar-width))]'
+		: ''} w-full max-w-full flex flex-col"
 	id="chat-container"
 >
 	{#if !loading}
