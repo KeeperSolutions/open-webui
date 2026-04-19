ab1c4c2b3a7f7439a2fdf3aceffad293c41881c5
Timothy Jaeryang Baek <tim@openwebui.com>
2026-04-19 22:50:08 +0900

    refac

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -946,8 +946,11 @@
 					/>
 				</a>
 
-				<a href="/chat" class="flex flex-1 px-1.5" on:click={newChatHandler}>
-					<div class=" self-center font-medium text-gray-850 dark:text-white font-primary">
+				<a href="/chat" class="flex flex-1 px-0.5" on:click={newChatHandler}>
+					<div
+						id="sidebar-webui-name"
+						class=" self-center font-medium text-gray-850 dark:text-white font-primary"
+					>
 						{$WEBUI_NAME}
 					</div>
 				</a>
