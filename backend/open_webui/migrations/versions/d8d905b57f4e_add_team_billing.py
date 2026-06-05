14bb08dc43cc027a6673b3ac7649545c9f77e34c
Kristijan  Bošnjak <kristijanbosnjak@MBPodKristijan.home>
2026-06-05 14:14:51 +0200

    migrations update

--- backend/open_webui/migrations/versions/d8d905b57f4e_add_team_billing.py
+++ backend/open_webui/migrations/versions/d8d905b57f4e_add_team_billing.py
@@ -1,6 +1,6 @@
 """Add team billing tables and team_id to stripe_billing
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: d8d905b57f4e
 Revises: f1a2b3c4d5e7
 Create Date: 2026-05-20 00:00:00.000000
 
@@ -11,7 +11,7 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "a1b2c3d4e5f6"
+revision: str = "d8d905b57f4e"
 down_revision: Union[str, None] = "f1a2b3c4d5e7"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
