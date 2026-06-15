7aec57ce2c377b0666d8f189cc7e90f609d9add6
Antonio Hadrović <106669301+ahadrovic-2@users.noreply.github.com>
2026-06-15 14:20:21 +0200

    STAGING: feature/TRAU-501 (#70)
    * Add hubgate pixel pattern dark
    
    Add ligth/dark pattern into chat routes
    
    * Add copilot feedback

--- src/routes/(app)/chat/+page.svelte
+++ src/routes/(app)/chat/+page.svelte
@@ -12,4 +12,20 @@
 	});
 </script>
 
-<Chat />
+<img
+	src="/hubgate/hubgate-pixel-pattern.svg"
+	alt=""
+	aria-hidden="true"
+	class="pointer-events-none dark:hidden"
+	style="position:fixed;top:0;left:0;width:100%;height:100%;object-fit:cover;z-index:0"
+/>
+<img
+	src="/hubgate/hubgate-pixel-pattern-dark.svg"
+	alt=""
+	aria-hidden="true"
+	class="pointer-events-none hidden dark:block"
+	style="position:fixed;top:0;left:0;width:100%;height:100%;object-fit:cover;z-index:0"
+/>
+<div class="relative" style="z-index:1">
+	<Chat />
+</div>
