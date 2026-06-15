93329ce79f876ab7251a878be1a3d601441a4ffd
ahadrovic-2 <antonio.hadrovic@keepersolutions.com>
2026-06-15 13:23:10 +0200

    Add hubgate pixel pattern dark
    Add ligth/dark pattern into chat routes

--- src/routes/(app)/chat/+page.svelte
+++ src/routes/(app)/chat/+page.svelte
@@ -12,4 +12,18 @@
 	});
 </script>
 
+<img
+	src="/hubgate/hubgate-pixel-pattern.svg"
+	alt=""
+	aria-hidden="true"
+	class="pointer-events-none dark:hidden"
+	style="position:fixed;top:0;left:0;width:100%;height:100%;object-fit:cover;z-index:0"
+/>
+<img
+	src="/hubgate/hubgate-pixel-pattern-dark.svg"
+	alt=""
+	aria-hidden="true"
+	class="pointer-events-none hidden dark:block"
+	style="position:fixed;top:0;left:0;width:100%;height:100%;object-fit:cover;z-index:0"
+/>
 <Chat />
