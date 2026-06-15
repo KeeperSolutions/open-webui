d447ed4b370ed58e3af53b46c58447fb94afad02
ahadrovic-2 <antonio.hadrovic@keepersolutions.com>
2026-06-15 16:58:35 +0200

    Add chat background component

---
+++ src/lib/components/chat/ChatBackground.svelte
@@ -0,0 +1,14 @@
+<img
+	src="/hubgate/hubgate-pixel-pattern.svg"
+	alt="Pixel pattern"
+	aria-hidden="true"
+	class="pointer-events-none dark:hidden"
+	style="position:fixed;top:0;left:0;width:100%;height:100%;object-fit:cover;z-index:0"
+/>
+<img
+	src="/hubgate/hubgate-pixel-pattern-dark.svg"
+	alt="Dark pixel pattern"
+	aria-hidden="true"
+	class="pointer-events-none hidden dark:block"
+	style="position:fixed;top:0;left:0;width:100%;height:100%;object-fit:cover;z-index:0"
+/>
--- src/routes/(app)/chat/+page.svelte
+++ src/routes/(app)/chat/+page.svelte
@@ -3,6 +3,7 @@
 	import { toast } from 'svelte-sonner';
 
 	import Chat from '$lib/components/chat/Chat.svelte';
+	import ChatBackground from '$lib/components/chat/ChatBackground.svelte';
 	import { page } from '$app/stores';
 
 	onMount(() => {
@@ -12,20 +13,5 @@
 	});
 </script>
 
-<img
-	src="/hubgate/hubgate-pixel-pattern.svg"
-	alt=""
-	aria-hidden="true"
-	class="pointer-events-none dark:hidden"
-	style="position:fixed;top:0;left:0;width:100%;height:100%;object-fit:cover;z-index:0"
-/>
-<img
-	src="/hubgate/hubgate-pixel-pattern-dark.svg"
-	alt=""
-	aria-hidden="true"
-	class="pointer-events-none hidden dark:block"
-	style="position:fixed;top:0;left:0;width:100%;height:100%;object-fit:cover;z-index:0"
-/>
-<div class="relative" style="z-index:1">
-	<Chat />
-</div>
+<ChatBackground />
+<Chat />
