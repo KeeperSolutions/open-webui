e3de19f4dfd3d5fbe6477f8829ada34803f6ec57
Maks Patafta <maks.patafta@keepersolutions.com>
2026-07-02 15:23:41 +0200

    feature/TRAU-519: add HgTrustPill and HgTrustMarquee

---
+++ src/lib/components/hubgate/HgTrustMarquee.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { TrustPill } from '$lib/data/trust-centre';
+	import HgTrustPill from './HgTrustPill.svelte';
+	export let pills: TrustPill[];
+</script>
+
+<div class="w-full border-y border-hg-border bg-hg-bg-surface">
+	<div class="max-w-[1248px] mx-auto px-4 sm:px-6 py-4 flex flex-wrap justify-center gap-x-8 gap-y-3">
+		{#each pills as pill}
+			<HgTrustPill icon={pill.icon} label={pill.label} />
+		{/each}
+	</div>
+</div>
